2867667f96b7d0d389535701c058bfababc3e83d
MarianaFuentesC <mfuentescr@unbosque.edu.co>
2026-04-09 08:35:45 -0500

    variable

---
+++ taller.js
@@ -0,0 +1,20 @@
+let nombrecompleto="";
+let edad=0;
+let tipodedocumento="";
+let numerodedocumento="";
+
+let salario=0;
+let comisiones="";
+let totaldehorasextra="";
+let clasificaciondenivelderiesgo="";
+
+const salariominimolegalvigente=1750905;
+const salariominimointegralvigente=22761765;
+const subsidiodetransporte=249095;
+const unidaddevalortributario=5237;
+
+const riesgominimo=0.522;
+const riesgobajo=1.044;
+const riesgomedio=2.436;
+const riesgoalto=4.350;
+const riesgomaximo=6.960;
